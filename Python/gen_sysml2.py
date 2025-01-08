4cc02a392604f8e64a01cc1fbb5e77b497869707
Hans Peter de Koning <hanspeter.dekoning@dekonsult.com>
2025-01-08 23:27:23 +0100

    fix: Cleaned up gen_sysml2.py with minor corrections

--- Python/gen_sysml2.py
+++ Python/gen_sysml2.py
@@ -3,7 +3,6 @@ import csv
 
 INDENT = "    "
 
-
 class Attribute:
     def __init__(self, kind: str, name: str, value: str):
         self.kind = kind
@@ -11,7 +10,7 @@ class Attribute:
         self.value = value
 
 class Node:
-    def __init__(self, short_name):
+    def __init__(self, short_name: str):
         self.short_name: str = short_name
         self.name: str = ""
         self.children: list[Node] = list()
@@ -35,7 +34,7 @@ class Node:
         buffer.append(f"{level*INDENT}}}\n")
 
     def __repr__(self):
-        return f'<Node: {self.short_name}>'
+        return f"<Node: {self.short_name}>"
 
 class MassPropertiesModel:
     def __init__(self):
@@ -43,7 +42,6 @@ class MassPropertiesModel:
         self.node_dict : dict[str, Node] = dict()
 
     def read_edge_list(self, edge_list_file_path: str):
-        root_node = None
         with open(edge_list_file_path, encoding="UTF8") as edge_list_file:
             csv_reader = csv.reader(edge_list_file, delimiter=',')
             header = next(csv_reader)
@@ -69,21 +67,20 @@ class MassPropertiesModel:
                 print(f"key={key} children={item.children}")
 
     def read_node_details(self, node_details_file_path: str):
-        root_node = None
         with open(node_details_file_path, encoding="UTF8") as node_details_file:
             csv_reader = csv.reader(node_details_file, delimiter='\t')
             header = next(csv_reader)
-            print("node_details_file header=", header)
+            print("DEBUG: node_details_file header=", header)
 
             for record in csv_reader:
                 map = dict(zip(header, record))
-                # print(f"map={map}")
+                # print(f"DEBUG: map={map}")
                 node = self.node_dict.get(map["id"])
                 if node:
                     node.name = map["name"]
                     node.add_attribute("String", 'POIconv', f"\"{map['POIconv']}\"")
-                    value = "NA" if map['Ipoint'] == "NA" else map['Ipoint'].lower()
-                    node.add_attribute("Boolean", 'Ipoint', value)
+                    IPoint_value = "NA" if map['Ipoint'] == "NA" else map['Ipoint'].lower()
+                    node.add_attribute("Boolean", 'Ipoint', IPoint_value)
                     for attr_name in ['mass', 'σ_mass', 'Cx', 'σ_Cx', 'Cy', 'σ_Cy', 'Cz', 'σ_Cz', 'Ixx', 'σ_Ixx', 'Iyy', 'σ_Iyy', 'Izz', 'σ_Izz', 'Ixy', 'σ_Ixy', 'Ixz', 'σ_Ixz', 'Iyz', 'σ_Iyz']:
                         node.add_attribute("Real", attr_name, map[attr_name])
 
@@ -107,5 +104,4 @@ if __name__ == "__main__":
     mass_properties_model = MassPropertiesModel()
     mass_properties_model.read_edge_list("../el-ok.csv")
     mass_properties_model.read_node_details("../mp-input.tsv")
-
     mass_properties_model.write_sysml2_model("MassPropertiesModel.sysml")
